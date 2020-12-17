d2710d78555083800605746d520f2f39d15ce47a
Wenjian Bi <wenjian.bi@gmail.com>
2020-12-17 15:18:56 -0600

    Update POLMM.R

--- R/POLMM.R
+++ R/POLMM.R
@@ -123,6 +123,7 @@ POLMM = function(objNull,
     }
   }
   
+  chrVec = gsub("^0", "", chrVec)   # added on 12-17-2020, in case that chrVec = "01".
   if(length(chrVec)==1){
     message(paste0("Chromosome of all markers in analysis are ",chrVec,"."))
     chrVec = rep(chrVec, m)
